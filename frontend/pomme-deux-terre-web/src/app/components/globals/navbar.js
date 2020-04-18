04c3acf7d69315c137696d90853470b0dcd411a2
Muhammad Muzamir <mu-za-mir@hotmail.com>
2020-04-18 18:15:24 +0200

    designed navbar

--- frontend/pomme-deux-terre-web/src/app/components/globals/navbar.js
+++ frontend/pomme-deux-terre-web/src/app/components/globals/navbar.js
@@ -24,98 +24,113 @@ function NavBarHead(props) {
   return (
     <React.Fragment>
       {CatshortList ? (
-        <Row>
-          <Col sm={2} className="side"></Col>
+        <React.Fragment>
+          <Row className="navbar-toprow">
+            <Col sm={2} className="side"></Col>
 
-          <Col sm={8} className="main">
-            <Navbar
-              collapseOnSelect
-              expand="lg"
-              variant="light"
-              style={{ padding: "0px 0px" }}
-            >
-              <Image
-                className="navbar-icon"
-                src={require("../../../assets/Apple.ico")}
-                rounded
-              />
-              <Navbar.Brand>
-                <Link className="nav-link" to={"/"} replace>
-                  Pomme Deux Terre
-                </Link>
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Link className="nav-link" to={"/"} replace>
-                    Home
+            <Col className="Container">
+              <Navbar className="row navbar-title">
+                {" "}
+                <Image
+                  style={{ maxHeight: "38px" }}
+                  className=""
+                  src={require("../../../assets/AppleTop.ico")}
+                />
+                Apple Two Earth{" "}
+                <p className="quotes-tag"> - Earth Wind Fire Water</p>
+              </Navbar>
+
+              <Navbar
+                className="row navbar-menu"
+                collapseOnSelect
+                expand="lg"
+                variant="light"
+                style={{ padding: "0px 0px" }}
+              >
+                <Navbar.Brand>
+                  <Image
+                    style={{ maxHeight: "35px" }}
+                    className="image-bottom"
+                    src={require("../../../assets/AppleBottom3.ico")}
+                  />
+                  <Link className="brand-title nav-link" to={"/"} replace>
+                    Pomme Deux Terre
                   </Link>
-                  <Nav>
-                    <Link className="nav-link" to={"/aboutus"} replace>
-                      About Us
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Link className="nav-link" to={"/"} replace>
+                      Home
                     </Link>
+                    <Nav>
+                      <Link className="nav-link" to={"/aboutus"} replace>
+                        About Us
+                      </Link>
+                    </Nav>
+                    <NavDropdown title="Category" id="collasible-nav-dropdown">
+                      {CatshortList.map((category) => {
+                        return (
+                          <Link
+                            key={category.id}
+                            className="dropdown-item"
+                            to={"/category?category=" + category.id}
+                          >
+                            {category.title}
+                          </Link>
+                        );
+                      })}
+                    </NavDropdown>
                   </Nav>
-                  <NavDropdown title="Category" id="collasible-nav-dropdown">
-                    {CatshortList.map((category) => {
-                      return (
-                        <Link
-                          key={category.id}
-                          className="dropdown-item"
-                          to={"/category?category=" + category.id}
-                        >
-                          {category.title}
-                        </Link>
-                      );
-                    })}
-                  </NavDropdown>
-                </Nav>
-                <Nav style={{ alignItems: "center" }}>
-                  {props.cookies.get("userId") ? (
-                    <React.Fragment>
-                      <Navbar className="solo-nav">
-                        <Link
-                          className="nav-link"
-                          to={"/user/" + props.cookies.get("userId")}
-                        >
-                          Profile
-                        </Link>
-                      </Navbar>
-                      <Navbar className="solo-nav">
-                        <Link
-                          className="nav-link"
-                          onClick={releaseUserDispatch}
-                          to={"/login"}
-                          replace
-                        >
-                          Logout
-                        </Link>
-                      </Navbar>
-                    </React.Fragment>
-                  ) : (
-                    <React.Fragment>
-                      <Navbar>
-                        <Link
-                          className="nav-link"
-                          to={"/login/?signup=true"}
-                          replace
-                        >
-                          Signup
-                        </Link>
-                      </Navbar>
-                      <Navbar>
-                        <Link className="nav-link" to={"/login"} replace>
-                          LogIn
-                        </Link>
-                      </Navbar>
-                    </React.Fragment>
-                  )}
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </Col>
+                  <Nav style={{ alignItems: "center" }}>
+                    {props.cookies.get("userId") ? (
+                      <React.Fragment>
+                        <Navbar className="solo-nav">
+                          <Link
+                            className="nav-link"
+                            to={"/user/" + props.cookies.get("userId")}
+                          >
+                            Profile
+                          </Link>
+                        </Navbar>
+                        <Navbar className="solo-nav">
+                          <Link
+                            className="nav-link"
+                            onClick={releaseUserDispatch}
+                            to={"/login"}
+                            replace
+                          >
+                            Logout
+                          </Link>
+                        </Navbar>
+                      </React.Fragment>
+                    ) : (
+                      <React.Fragment>
+                        <Navbar>
+                          <Link
+                            className="nav-link"
+                            to={"/login/?signup=true"}
+                            replace
+                          >
+                            Signup
+                          </Link>
+                        </Navbar>
+                        <Navbar>
+                          <Link className="nav-link" to={"/login"} replace>
+                            LogIn
+                          </Link>
+                        </Navbar>
+                      </React.Fragment>
+                    )}
+                  </Nav>
+                </Navbar.Collapse>
+              </Navbar>
+            </Col>
 
-          <Col sm={2} className="side"></Col>
-        </Row>
+            {/* </Col> */}
+            <Col sm={2} className="side"></Col>
+          </Row>
+        </React.Fragment>
       ) : null}
     </React.Fragment>
   );
